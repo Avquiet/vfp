2fbaaa072611c62adf11d290b797590be2a8201a
Avquiet <78691610+Avquiet@users.noreply.github.com>
2021-09-17 16:23:47 +0300

    added 1-work

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <input
+      type="text"
+      placeholder="text"
+      @blur="blurMethod"
+      @focus="focusMethod"
+      @keydown="blurMethod"
+      @keyup="focusMethod"
+    />
+    <button @click="c = a + b">Click</button>
+    <button @click="doThat('Hello', $event)">Clear</button>
+    {{ c }}
+    <calc />
   </div>
 </template>
 
 <script>
+import Calc from './components/CalcBest.vue'
 import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Calc
+  },
+  data: () => ({
+    show: false,
+    c: 0,
+    a: 1,
+    b: 1
+  }),
+  methods: {
+    doThat (str, event) {
+      console.log(str, event)
+      this.c = 0
+    }
   }
 }
 </script>
@@ -24,5 +49,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-bottom: 20px;
 }
 </style>
---
+++ src/components/CalcBest.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class='calc'>
+    <div class="main">
+      <input v-model="op1" type="number" />
+      <input v-model="op2" type="number" />
+      <h3>= {{ result }}</h3>
+    </div>
+    <div class="keyboard">
+      <button @click="plus">+</button>
+      <button @click="sub">-</button>
+      <button @click="div">/</button>
+      <button @click="multi">*</button>
+      <button @click="degree">^</button>
+      <button @click="integer">%</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  data () {
+    return {
+      op1: '',
+      op2: '',
+      result: ''
+    }
+  },
+  methods: {
+    integer () {
+      const { op1, op2 } = this
+      this.result = op1 % op2
+    },
+    degree () {
+      const { op1, op2 } = this
+      this.result = op1 ** op2
+    },
+    plus () {
+      const { op1, op2 } = this
+      this.result = op1 + op2
+    },
+    sub () {
+      const { op1, op2 } = this
+      this.result = op1 - op2
+    },
+    div () {
+      const { op1, op2 } = this
+      this.result = op1 / op2
+    },
+    multi () {
+      const { op1, op2 } = this
+      this.result = op1 * op2
+    }
+  }
+}
+</script>
